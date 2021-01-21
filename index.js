05e965e1525418f3e840ee0416f0b7d34a8c9da8
zmjdev <77663245+zmjdev@users.noreply.github.com>
2021-01-21 23:31:10 +0530

    map function implemented from scratch

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const newArray = numbers.map((number) => number + 1);
-
 console.log(newArray);
+
+function addOne(number) {
+	return number + 1;
+}
+
+function map(array, instruction) {
+	const output = [];
+	for (let i = 0; i < array.length; i++) {
+		output.push(instruction(array[1]));
+	}
+	return output;
+}
