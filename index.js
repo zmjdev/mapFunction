f2bf45519bd25e978fab37d5ccd720008aaf99e4
zmjdev <77663245+zmjdev@users.noreply.github.com>
2021-01-21 23:26:02 +0530

    implemented arrow function

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function addOne(number) {
-	return number + 1;
-}
-
-const newArray = numbers.map(addOne);
+const newArray = numbers.map((number) => number + 1);
 
 console.log(newArray);
